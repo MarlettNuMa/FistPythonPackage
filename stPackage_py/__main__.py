948ea6997db1150d06b68c6cfdb54038b7d67b29
Marlett NuMa <marlett.numa@gmail.com>
2022-06-01 18:04:38 -0500

    Modulo logging

--- stPackage_py/__main__.py
+++ stPackage_py/__main__.py
@@ -1,9 +1,25 @@
+import logging
 from stPackage_py import unreleased         #con unreleased inicializada en init
 
+"""
+MESSAGE INTO LOGGING
+1)INFO
+2)DEBUG
+3)WARNING
+4)ERROR
+5)CRITICAL
+
+logging.basicConfig(level=logging.DEBUG)
+    muestra mensajes de 20 a superiores
+"""
+
+logging.basicConfig(level = logging.DEBUG)
+
+
 if __name__ == '__main__':
-    print('>>>  Ejecución del paquete iniciada')
+    logging.debug('>>>  Ejecución del paquete iniciada')
     
     workshops = unreleased()
     print(workshops)
 
-    print ('>>> Finalizada la ejecución del paquete')
+    logging.debug('>>> Finalizada la ejecución del paquete')
